f409c87f90a0b4bc1623776228af0a6fd3814c6c
sihams <aitoumgharsiham@gmail.com>
2024-10-31 21:37:01 +0100

    ajouter les exercices

--- tp1/AfficherArguments.java
+++
@@ -1,8 +0,0 @@
- public class AfficherArguments {
-        public static void main(String[] args) {
-            System.out.println("Nombre d'arguments : " + args.length);
-            for (int i = 0; i < args.length; i++) {
-                    System.out.println("Argument " + i + " : " + args[i]);
- }
- }
- }
